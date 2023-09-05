5819fd30bd0a6fe9f4aa8373eaf9ddc65fc34dd1
mgendrot2023 <mgendrot2023@129SE23-451MMS3.ad.campus-eni.fr>
2023-09-05 14:33:49 +0200

    bo enchere retrait et categorie

--- src/main/java/trocenchere/bo/Categorie.java
+++ src/main/java/trocenchere/bo/Categorie.java
@@ -1,5 +1,53 @@
 package trocenchere.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categorie {
 
+	private int id_categorie;
+	private String libelle;
+	private List<ArticleVendu>listeArticlesVendus = new ArrayList<ArticleVendu>();
+	
+	public Categorie() {}
+
+	public Categorie(int id_categorie, String libelle) {
+		super();
+		this.id_categorie = id_categorie;
+		this.libelle = libelle;
+	}
+
+	public int getId_categorie() {
+		return id_categorie;
+	}
+
+	public void setId_categorie(int id_categorie) {
+		this.id_categorie = id_categorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public List<ArticleVendu> getListeArticlesVendus() {
+		return listeArticlesVendus;
+	}
+
+	public void setListeArticlesVendus(List<ArticleVendu> listeArticlesVendus) {
+		this.listeArticlesVendus = listeArticlesVendus;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorie [id_categorie=" + id_categorie + ", libelle=" + libelle + ", listeArticlesVendus="
+				+ listeArticlesVendus + "]";
+	}
+	
+	
+	
+	
 }
--- src/main/java/trocenchere/bo/Enchere.java
+++ src/main/java/trocenchere/bo/Enchere.java
@@ -1,5 +1,61 @@
 package trocenchere.bo;
 
-public class Enchere {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Enchere {
+	
+	private LocalDate date_enchere;
+	private int montant_enchere;
+	private List<Utilisateur>listeUtilisateurs = new ArrayList <Utilisateur>();
+	private List<ArticleVendu>listeArticlesVendus = new ArrayList<ArticleVendu>();
+	
+	public Enchere() {}
+
+	public Enchere(LocalDate date_enchere, int montant_enchere) {
+		super();
+		this.date_enchere = date_enchere;
+		this.montant_enchere = montant_enchere;
+	}
+
+	public LocalDate getDate_enchere() {
+		return date_enchere;
+	}
+
+	public void setDate_enchere(LocalDate date_enchere) {
+		this.date_enchere = date_enchere;
+	}
+
+	public int getMontant_enchere() {
+		return montant_enchere;
+	}
+
+	public void setMontant_enchere(int montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+
+	public List<Utilisateur> getListeUtilisateurs() {
+		return listeUtilisateurs;
+	}
+
+	public void setListeUtilisateurs(List<Utilisateur> listeUtilisateurs) {
+		this.listeUtilisateurs = listeUtilisateurs;
+	}
+
+	public List<ArticleVendu> getListeArticlesVendus() {
+		return listeArticlesVendus;
+	}
+
+	public void setListeArticlesVendus(List<ArticleVendu> listeArticlesVendus) {
+		this.listeArticlesVendus = listeArticlesVendus;
+	}
+
+	@Override
+	public String toString() {
+		return "Enchere [date_enchere=" + date_enchere + ", montant_enchere=" + montant_enchere + ", listeUtilisateurs="
+				+ listeUtilisateurs + ", listeArticlesVendus=" + listeArticlesVendus + "]";
+	}
+	
+	
 }
