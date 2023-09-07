1ba3f6764274e7651791e8780c641f159ae226aa
eachard2023 <eachard2023@129SE23-861MMS3.ad.campus-eni.fr>
2023-09-07 12:23:01 +0200

    Changement dans la vente des articles

--- src/main/java/trocenchere/dal/ArticleDAO.java
+++ src/main/java/trocenchere/dal/ArticleDAO.java
@@ -1,16 +1,12 @@
 package trocenchere.dal;
 
-<<<<<<< HEAD
+
 import trocenchere.bo.Article;
-=======
 import java.util.List;
 
-import trocenchere.bo.Article;
-import trocenchere.bo.Enchere;
->>>>>>> branch 'master' of https://github.com/mehdi35000/trocenchere.git
-
 public interface ArticleDAO {
-	void insert(Article article);
 
 	List<Article> selectAllArticlesEnVente();
+	
+	void insert(Article article); //throws BusinessException
 }
