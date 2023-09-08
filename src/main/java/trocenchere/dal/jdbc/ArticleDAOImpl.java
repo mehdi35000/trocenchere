c2734f19af81b6110b99f7607d495572b83cd312
eachard2023 <eachard2023@129SE23-861MMS3.ad.campus-eni.fr>
2023-09-08 12:00:52 +0200

    Affichage du pseudo par l'utilisateur.

--- src/main/java/trocenchere/dal/jdbc/ArticleDAOImpl.java
+++ src/main/java/trocenchere/dal/jdbc/ArticleDAOImpl.java
@@ -13,12 +13,10 @@ import trocenchere.bo.Utilisateur;
 import trocenchere.dal.ArticleDAO;
 
 public class ArticleDAOImpl implements ArticleDAO{
-<<<<<<< HEAD
-=======
 	private final static String INSERT_ARTICLE ="INSERT INTO ARTICLES (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente, id_utilisateur, id_categorie)"; 
->>>>>>> branch 'master' of https://github.com/mehdi35000/trocenchere.git
 
-	private final static String SELECT_ALL_ARTICLESENVENTE = """
+
+	/*private final static String SELECT_ALL_ARTICLESENVENTE = """
 			SELECT
     articles.nom_article,
     articles.description,
@@ -26,58 +24,81 @@ public class ArticleDAOImpl implements ArticleDAO{
     articles.date_fin_encheres,
     articles.prix_initial,
     articles.prix_vente,
-    utilisateurs.pseudo AS pseudo_proprietaire
-FROM ARTICLES INNER JOIN UTILISATEURS ON articles.id_utilisateur = utilisateurs.id_utilisateur;"""; //WHERE articles.prix_vente IS NULL;""";
+    utilisateurs.pseudo 
+FROM ARTICLES INNER JOIN UTILISATEURS ON articles.id_utilisateur = utilisateurs.id_utilisateur;"""; //WHERE articles.prix_vente IS NULL;"""; // à changer pour un truc qui combine les deux
+*/
+	private final static String SELECT_ALL_ARTICLES_A_VENDRE = """
+			SELECT * FROM UTILISATEURS RIGHT JOIN ARTICLES ON utilisateurs.id_utilisateur=articles.id_utilisateur;
+			""";
+	
+	@Override
+	public List<Article> selectAllArticlesEnVente() {
+	    List<Article> articlesEnVente = new ArrayList<>();
 
+	    try (Connection cnx = ConnectionProvider.getConnection()) {
+	        Statement stmt = cnx.createStatement();
+	        ResultSet rs = stmt.executeQuery(SELECT_ALL_ARTICLES_A_VENDRE);
+
+	        while (rs.next()) {
+	        	//permet créer une nouvelle instance d'Article pour chaque enregistrement de la base de données
+	        	//afin de les mettre dans une liste articlesEnVente
+	            Article article = new Article(); 
+	            article.setNom_article(rs.getString("nom_article"));
+	            article.setDate_debut_encheres(rs.getDate("date_debut_encheres").toLocalDate());
+	            article.setDate_fin_encheres(rs.getDate("date_fin_encheres").toLocalDate());
+	            article.setMise_a_prix(rs.getInt("prix_initial"));
+	            article.setPrix_vente(rs.getInt("prix_vente"));           
+	            
+	            Utilisateur vendeur = new Utilisateur();
+	            vendeur.setId_utilisateur(rs.getInt("id_utilisateur"));
+	            vendeur.setPseudo(rs.getString("pseudo"));
+	            vendeur.setNom(rs.getString("nom"));
+	            vendeur.setPrenom(rs.getString("prenom"));
+	            vendeur.setEmail(rs.getString("email"));
+	            vendeur.setTelephone(rs.getString("telephone"));
+	            vendeur.setRue(rs.getString("rue"));
+	            vendeur.setCode_postal(rs.getString("code_postal"));
+	            vendeur.setVille(rs.getString("ville"));
+	            vendeur.setMot_de_passe(rs.getString("mot_de_passe"));
+	            vendeur.setCredit(rs.getInt("credit"));
+	            vendeur.setAdministrateur(rs.getBoolean("administrateur"));
+
+	            article.setUtilisateur(vendeur);
+	            //UtilisateurDAOImpl dao = new UtilisateurDAOImpl();//pour récupérer les infos de Flavie 
+	            //Utilisateur vendeur = dao.selectById( rs.getString("id_utilisateur")); //pour récupèrer l'id de l'utilisateur qui a mis l'article en vente pour avoir le pseudo associé au vendeur.
+	            //utilisateur.getPseudo ?? 
+	            
+	            /*
+                String pseudoVendeur = rs.getString("pseudo_proprietaire");
+                Utilisateur vendeur = new Utilisateur();
+                vendeur.setPseudo(pseudoVendeur);
+                //article.setUtilisateur(vendeur);
+                System.out.println(pseudoVendeur);
+                System.out.println(article);
+                System.out.println(articlesEnVente);
+                
+                article.setUtilisateur(vendeur);
+                */
+
+	            articlesEnVente.add(article);	
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+
+	    return articlesEnVente;
+	    
+	}
+	
+	public void insert(Article article) {
+		
+		try (Connection cnx = ConnectionProvider.getConnection()){
+		PreparedStatement pstmt = cnx.prepareStatement(INSERT_ARTICLE, PreparedStatement.RETURN_GENERATED_KEYS);	
 			
-			@Override
-			public List<Article> selectAllArticlesEnVente() {
-			    List<Article> articlesEnVente = new ArrayList<>();
-
-			    try (Connection cnx = ConnectionProvider.getConnection()) {
-			        Statement stmt = cnx.createStatement();
-			        ResultSet rs = stmt.executeQuery(SELECT_ALL_ARTICLESENVENTE);
-
-			        while (rs.next()) {
-			        	//permet créer une nouvelle instance d'Article pour chaque enregistrement de la base de données
-			        	//afin de les mettre dans une liste articlesEnVente
-			            Article article = new Article(); 
-			            article.setNom_article(rs.getString("nom_article"));
-			            
-			            article.setDate_debut_encheres(rs.getDate("date_debut_encheres").toLocalDate());
-			            article.setDate_fin_encheres(rs.getDate("date_fin_encheres").toLocalDate());
-			            article.setMise_a_prix(rs.getInt("prix_initial"));
-			            article.setPrix_vente(rs.getInt("prix_vente"));
-			            //article
-			            
-			            // Récupérer le pseudo du propriétaire depuis le champ "pseudo_proprietaire"
-		                String pseudoVendeur = rs.getString("pseudo_proprietaire");
-		                Utilisateur vendeur = new Utilisateur();
-		                vendeur.setPseudo(pseudoVendeur);
-		                //article.setUtilisateur(vendeur);
-		                System.out.println(pseudoVendeur);
-		                System.out.println(article);
-		                System.out.println(articlesEnVente);
-
-			            articlesEnVente.add(article);	
-			        }
-			    } catch (SQLException e) {
-			        e.printStackTrace();
-			    }
-
-			    return articlesEnVente;
-			    
-			}
-			
-			public void insert(Article article) {
-				
-				try (Connection cnx = ConnectionProvider.getConnection()){
-				PreparedStatement pstmt = cnx.prepareStatement(INSERT_ARTICLE, PreparedStatement.RETURN_GENERATED_KEYS);	
-					
-				}catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
