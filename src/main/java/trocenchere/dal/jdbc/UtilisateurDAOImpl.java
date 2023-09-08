cc04f4a5b0b609a78e8a205fc927ac762693eaf9
flhelgoualch2023 <flhelgoualch2023@129SE23-CD4MMS3.ad.campus-eni.fr>
2023-09-08 14:11:57 +0200

    Maj UtDAOIMPL

--- src/main/java/trocenchere/dal/jdbc/UtilisateurDAOImpl.java
+++ src/main/java/trocenchere/dal/jdbc/UtilisateurDAOImpl.java
@@ -8,90 +8,87 @@ import java.sql.SQLException;
 import trocenchere.bo.Utilisateur;
 import trocenchere.dal.UtilisateurDAO;
 
-public class UtilisateurDAOImpl implements UtilisateurDAO{
+public class UtilisateurDAOImpl implements UtilisateurDAO {
 
 	private final static String SELECT_CONNEXION = "SELECT id_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur "
-													+ "FROM UTILISATEURS WHERE pseudo = ? AND mot_de_passe = ?";
-	
+			+ "FROM UTILISATEURS WHERE pseudo = ? AND mot_de_passe = ?";
+
 	private final static String INSERT_UTILISATEUR = "INSERT INTO UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) VALUES (?,?,?,?,?,?,?,?,?,?,?);";
-			
+
 	private final static String DELETE = "DELETE FROM UTILISATEURS WHERE id_utilisateur = ?;";
 
-@Override
-public void insert(Utilisateur nouvelUtilisateur) {
-	try (Connection cnx = ConnectionProvider.getConnection()){
-		
-		PreparedStatement pstmt = cnx.prepareStatement(INSERT_UTILISATEUR, PreparedStatement.RETURN_GENERATED_KEYS);
-		pstmt.setString(1,nouvelUtilisateur.getPseudo());
-		pstmt.setString(2,nouvelUtilisateur.getNom());
-		pstmt.setString(3,nouvelUtilisateur.getPrenom());
-		pstmt.setString(4,nouvelUtilisateur.getEmail());
-		pstmt.setString(5,nouvelUtilisateur.getTelephone());
-		pstmt.setString(6,nouvelUtilisateur.getRue());
-		pstmt.setString(7,nouvelUtilisateur.getCode_postal());
-		pstmt.setString(8,nouvelUtilisateur.getVille());
-		pstmt.setString(9,nouvelUtilisateur.getMot_de_passe());
-		pstmt.setInt(10,nouvelUtilisateur.getCredit());
-		pstmt.setBoolean(11,nouvelUtilisateur.isAdministrateur());
+	@Override
+	public void insert(Utilisateur nouvelUtilisateur) {
+		try (Connection cnx = ConnectionProvider.getConnection()) {
 
-		pstmt.executeUpdate();
-		
-		
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
+			PreparedStatement pstmt = cnx.prepareStatement(INSERT_UTILISATEUR, PreparedStatement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, nouvelUtilisateur.getPseudo());
+			pstmt.setString(2, nouvelUtilisateur.getNom());
+			pstmt.setString(3, nouvelUtilisateur.getPrenom());
+			pstmt.setString(4, nouvelUtilisateur.getEmail());
+			pstmt.setString(5, nouvelUtilisateur.getTelephone());
+			pstmt.setString(6, nouvelUtilisateur.getRue());
+			pstmt.setString(7, nouvelUtilisateur.getCode_postal());
+			pstmt.setString(8, nouvelUtilisateur.getVille());
+			pstmt.setString(9, nouvelUtilisateur.getMot_de_passe());
+			pstmt.setInt(10, nouvelUtilisateur.getCredit());
+			pstmt.setBoolean(11, nouvelUtilisateur.isAdministrateur());
 
-@Override
-public Utilisateur connexionUtilisateur(String pseudo, String mot_de_passe) {
-	
-	PreparedStatement pstmt  = null;
-	ResultSet rs = null;
-	Utilisateur utilisateur = null;
-	
-	try (Connection cnx = ConnectionProvider.getConnection()){
-		
-		pstmt = cnx.prepareStatement(SELECT_CONNEXION);
-		pstmt.setString(1,pseudo);
-		pstmt.setString(2,mot_de_passe);
-		
-		rs = pstmt.executeQuery();
-		
-		if (rs.next()) {
-		utilisateur = new Utilisateur ();
-		utilisateur.setId_utilisateur(rs.getInt ("id_utilisateur"));
-		utilisateur.setPseudo(rs.getString ("pseudo"));
-		utilisateur.setNom(rs.getString ("nom"));
-		utilisateur.setEmail(rs.getString ("email"));
-		utilisateur.setTelephone(rs.getString ("telephone"));
-		utilisateur.setRue(rs.getString ("rue"));
-		utilisateur.setCode_postal(rs.getString ("code_postal"));
-		utilisateur.setVille(rs.getString ("ville"));
-		utilisateur.setMot_de_passe(rs.getString ("mot_de_passe"));
-		utilisateur.setCredit(rs.getInt ("credit"));
-		utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
-			
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-	return utilisateur;
-}
 
-@Override
-public void delete(int id_utilisateur) {
-	try(Connection cnx = ConnectionProvider.getConnection()) {
-		PreparedStatement pStmt = cnx.prepareStatement(DELETE);
-		pStmt.setInt(1, id_utilisateur);
-		pStmt.executeUpdate();
-	} catch (SQLException e) {
-		e.printStackTrace();
+	@Override
+	public Utilisateur connexionUtilisateur(String pseudo, String mot_de_passe) {
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Utilisateur utilisateur = null;
+
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+
+			pstmt = cnx.prepareStatement(SELECT_CONNEXION);
+			pstmt.setString(1, pseudo);
+			pstmt.setString(2, mot_de_passe);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				utilisateur = new Utilisateur();
+				utilisateur.setId_utilisateur(rs.getInt("id_utilisateur"));
+				utilisateur.setPseudo(rs.getString("pseudo"));
+				utilisateur.setNom(rs.getString("nom"));
+				utilisateur.setEmail(rs.getString("email"));
+				utilisateur.setTelephone(rs.getString("telephone"));
+				utilisateur.setRue(rs.getString("rue"));
+				utilisateur.setCode_postal(rs.getString("code_postal"));
+				utilisateur.setVille(rs.getString("ville"));
+				utilisateur.setMot_de_passe(rs.getString("mot_de_passe"));
+				utilisateur.setCredit(rs.getInt("credit"));
+				utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return utilisateur;
 	}
-}
 
+	@Override
+	public void delete(int id_utilisateur) {
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+			PreparedStatement pStmt = cnx.prepareStatement(DELETE);
+			pStmt.setInt(1, id_utilisateur);
+			pStmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-	
 }
